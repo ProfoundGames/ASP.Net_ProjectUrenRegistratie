85a724bb932a1685de7f3733a76a1b6fa8a13763
5n0wD <kevmensink@hotmail.com>
2014-10-30 21:57:53 +0100

    begin gemaakt aan de login pagina
    code moet nog ff verder worden gerecycled, er staan comments in de code

--- ProjectUrenRegistratie/WebsiteItems/Login.aspx
+++ ProjectUrenRegistratie/WebsiteItems/Login.aspx
@@ -1,7 +1,5 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ProjectUrenRegistratie.WebsiteItems.WebForm1" %>
-
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ProjectUrenRegistratie.WebsiteItems.WebForm1" %>
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
@@ -9,14 +7,14 @@
 <body>
     <form id="form1" runat="server">
         <div>
-
+            <%--ff mooi maken met CSS ofzo--%>
             <asp:Label ID="lblUsername" runat="server" Text="Username"></asp:Label>
             <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
             <br />
             <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>
             <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
             <br />
-            <asp:Button ID="Button1" runat="server" Text="Button" />
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
 
         </div>
     </form>
--- ProjectUrenRegistratie/WebsiteItems/Login.aspx.cs
+++ ProjectUrenRegistratie/WebsiteItems/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Security.Cryptography;
 
 namespace ProjectUrenRegistratie.WebsiteItems
 {
@@ -16,7 +17,54 @@ namespace ProjectUrenRegistratie.WebsiteItems
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            UrenRegistratieEntities1 objUrenReg = new UrenRegistratieEntities1();
+            UrenRegistratieEntities1 EntityModel = new UrenRegistratieEntities1();
+
+            string userStr = txtUsername.Text.ToLower();
+            string passwordStr = CalculateHashedPassword(txtPassword.Text);
+
+            var Gebruiker = from g in EntityModel.Users
+                            where g.passwrd == passwordStr && g.username == userStr
+                            select new { ID = g.userId, naam = g.firstname, achternaam = g.lastname, rechten = g.permission, foto = g.photo };
+
+            if (Gebruiker.Any())
+            {
+                var UserIDGetter = (from uID in Gebruiker
+                                    select uID.ID);
+
+                foreach (int uID in UserIDGetter)
+                {
+                    userId = uID;
+                }
+
+                #region MOTD
+                var LoggedUser = (from LogUse in Gebruiker
+                                  select LogUse.naam);
+
+                foreach (string logUserName in LoggedUser)
+                {
+                    //Met javascript doen
+                    //MessageBox.Show("Welcome" + " " + logUserName + "\n Have a nice day.");
+                }
+                #endregion
+
+                //van hier moet je dus ingelogt zijn
+            }
+            else
+            {
+                // wachtwoord/user name is fout 
+            }
+
+        }
+
+        private static string CalculateHashedPassword(string clearpwd)
+        {
+            using (var sha = SHA256.Create())
+            {
+                //dit hier asap fixen
+                var computedHash = sha.ComputeHash(Encoding.Unicode.GetBytes(clearpwd));
+
+                return Convert.ToBase64String(computedHash);
+            }
         }
     }
 }
